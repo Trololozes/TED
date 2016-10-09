c075902fd02f3391b1176593329777495d64f476
Luigi Lazzari <luigi.lc89@gmail.com>
2016-10-09 18:23:36 -0300

    contrib

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2015  Pierluigi Lazzari
+ *  Copyright (C) 2015  Carlos, Luigi - Class of 2015
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
